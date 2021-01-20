6a2e6081462700afc069e1c312d64c360fe5374f
Christian Linder <christian.linder@gmail.com>
2021-01-21 00:34:16 +0100

    auto-update

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+git pull
 wget https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile -O impf.xlsx
 xlsx2csv -n Impfungen_proTag -f %Y-%m-%d impf.xlsx impf.csv
 node parse.mjs
