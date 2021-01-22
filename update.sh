cd0f502490ccc9764c84e16a87867a99d695a920
Christian Linder <chrischan@god.rczdcaodmpkzj4eg.myfritz.net>
2021-01-22 14:53:04 +0100

    auto-update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 git pull
 wget https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile -O impf.xlsx
 xlsx2csv -n Impfungen_proTag -f %Y-%m-%d impf.xlsx impf.csv
-node parse.mjs
+/home/chrischan/.nvm/versions/node/v14.15.4/bin/node parse.mjs
 git commit -a -m "auto-update"
 git push
